a94517f9c40f4487e7eb8a3ce3d948d9b083721a
estevaum <arthurestevao631@gmail.com>
2022-09-07 10:24:36 -0300

    add meta tags

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,24 @@
 import NavBar from '../components/NavBar'
 import backgroundImage from '../public/background.png'
-import CarouselTest from '../components/Carousel'
 import HeaderHome from '../components/HeaderHome'
 import BookCatalog from '../components/BookCatalog'
-import Footer from '../components/Footer'
+import Head from 'next/head'
 
 
 export default function Home() {
     return (
-        <div id="container" className='h-full bg-cover' style={{backgroundImage: 'url(/background.png)'}}>
-                <NavBar />
-                <main>
-                    <HeaderHome />
-                    <BookCatalog />
-                </main>
-        </div>
+        <>
+            <Head>
+                <title>Biblioteca ETEPAM</title>
+                       
+            </Head>
+            <div id="container" className='h-full bg-cover' style={{backgroundImage: 'url(/background.png)'}}>
+                    <NavBar />
+                    <main>
+                        <HeaderHome />
+                        <BookCatalog />
+                    </main>
+            </div>
+        </>
     )
 }
